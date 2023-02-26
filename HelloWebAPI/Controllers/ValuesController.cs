879b5fdecf3d180878822f630f275d6c9a302cb9
ekurniawan <erick.kurniawan@gmail.com>
2023-02-26 16:50:33 +0700

    first commit

---
+++ HelloWebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HelloWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        // GET: api/values
+        [Route("GetValue")]
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody]string value)
+        {
+            return Ok($"Data yang dikirimkan: {value}");
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
+
