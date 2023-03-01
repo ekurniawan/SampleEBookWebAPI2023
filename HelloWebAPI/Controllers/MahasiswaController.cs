00f7e09c4ab02321282b49fab8695c09bbebcfa5
ekurniawan <erick.kurniawan@gmail.com>
2023-03-01 21:38:14 +0700

    first commit

---
+++ HelloWebAPI/Controllers/MahasiswaController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HelloWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HelloWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class MahasiswaController : ControllerBase
+    {
+        // GET api/values 
+        [HttpGet]
+        public ActionResult<IEnumerable<Mahasiswa>> Get()
+        {
+            return new Mahasiswa[] {
+                new Mahasiswa{NIM = "123", Nama = "Nama123", Alamat =
+"Alamat123", Jurusan = "Jurusan123"},
+                new Mahasiswa{NIM = "234", Nama = "Nama234", Alamat =
+"Alamat234", Jurusan = "Jurusan234"},
+                new Mahasiswa{NIM = "345", Nama = "Nama345", Alamat =
+"Alamat345", Jurusan = "Jurusan345"}
+            };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")] 
+        public ActionResult<Mahasiswa> Get(string id)
+        {
+            return new Mahasiswa
+            {
+                NIM = "123",
+                Nama = "Nama123",
+                Alamat =
+"Alamat123",
+                Jurusan = "Jurusan123"
+            };
+        }
+
+        // POST api/values 
+        [HttpPost]
+        public void Post([FromBody] Mahasiswa value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")] 
+        public void Put(string id, [FromBody] Mahasiswa value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")] 
+        public void Delete(string id)
+        {
+        }
+
+    }
+}
+
---
+++ HelloWebAPI/Models/Mahasiswa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace HelloWebAPI.Models
+{
+	public class Mahasiswa
+	{
+        public string NIM { set; get; } = default!;
+        public string Nama { set; get; } = default!;
+        public string Alamat { set; get; } = default!;
+        public string Jurusan { set; get; } = default!;
+    }
+}
+
